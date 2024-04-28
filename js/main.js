8da5b203a599bafeda6701d77a6b2627df714bc4
Konstantin <168019285+3069603@users.noreply.github.com>
2024-04-28 23:02:37 +0300

    Modified files have been added
    - main.css;
    - media.css;
    - main.js

--- js/main.js
+++ js/main.js
@@ -7,5 +7,34 @@ navBtn.onclick = () => {
         navBtn.innerHTML = "&#9776;"; // HTML-символ открытия
     }
 };
+
+// JavaScript to hide the menu on link click
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        const nav = document.querySelector('#nav');
+        nav.classList.remove('open');
+        const navBtn = document.querySelector('#nav-btn');
+        navBtn.innerHTML = "&#9776;"; // Change back to hamburger icon
+    });
+});
+
+// JavaScript to show the button after scrolling below the services section
+window.onload = function() {
+    var servicesSection = document.getElementById("services");
+    var scrollButton = document.getElementById("scroll-to-top");
+    if (document.body.scrollTop > servicesSection.offsetTop || document.documentElement.scrollTop > servicesSection.offsetTop) {
+        scrollButton.style.display = "block";
+    }
+};
+window.onscroll = function() {
+    var servicesSection = document.getElementById("services");
+    var scrollButton = document.getElementById("scroll-to-top");
+    if (document.body.scrollTop > servicesSection.offsetTop || document.documentElement.scrollTop > servicesSection.offsetTop) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+};
+
 // подключение анимации
 AOS.init();
